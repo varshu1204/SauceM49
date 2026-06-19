33c13589ab06cd21919fab146e7de614f32adf48
QSP <QSP@DESKTOP-K5OA249>
2026-06-19 10:21:11 +0530

    commited

---
+++ src/test/java/Demo/Demo1Test.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test
+	public void movie1Test()
+	{
+		System.out.println("Nemo");
+	}
+}
---
+++ src/test/java/Demo/DemoTest.java
@@ -0,0 +1,27 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	
+@Test(groups = "smoke")
+public void movieTest()
+{
+	System.out.println("Swapped");
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
